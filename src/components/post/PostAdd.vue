486ad97283e4598f9803c0c16773bbb0cd52e076
吴金锋 <jinfeng.wu@edaibu.net>
2017-11-27 20:31:13 +0800

    分类目录

--- src/components/post/PostAdd.vue
+++ src/components/post/PostAdd.vue
@@ -7,7 +7,7 @@
       <el-input type="textarea" v-model="ruleForm.brief" class="brief"></el-input>
     </el-form-item>
     <el-form-item label="内容" prop="extended">
-      <mavon-editor v-model="value"/>
+      <mavon-editor v-model="ruleForm.extended"/>
     </el-form-item>
     <el-form-item label="置顶" prop="top">
       <el-checkbox-group v-model="ruleForm.top">
@@ -17,22 +17,13 @@
     <el-form-item label="分类" prop="postcategory">
       <el-checkbox-group v-model="ruleForm.postcategory">
         <el-checkbox label="美食" name="type"></el-checkbox>
-        <el-checkbox label="活动" name="type"></el-checkbox>
+        <el-checkbox label="活动2" name="type"></el-checkbox>
         <el-checkbox label="活动" name="type"></el-checkbox>
         <el-checkbox label="品牌" name="type"></el-checkbox>
       </el-checkbox-group>
     </el-form-item>
     <el-form-item label="图片" prop="image">
-      <el-upload
-        class="upload-demo"
-        action="https://jsonplaceholder.typicode.com/posts/"
-        :on-preview="handlePreview"
-        :on-remove="handleRemove"
-        :file-list="fileList2"
-        list-type="picture">
-        <el-button size="small" type="primary">点击上传</el-button>
-        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-      </el-upload>
+      <el-input v-model="ruleForm.image" class="image" placeholder="图片uri"></el-input>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="submitForm('ruleForm')">创建</el-button>
@@ -54,25 +45,44 @@
           extended: '',
           image: ''
         },
-        value: '', // 编辑器
         rules: {
           title: [
             {required: true, message: '请输入标题', trigger: 'blur'},
             {min: 1, max: 20, message: '长度在 1 到 20 个字符', trigger: 'blur'}
+          ],
+          extended: [
+            {required: true, message: '请输入内容', trigger: 'blur'}
           ]
-        },
-        fileList2: [{
-          name: 'food.jpeg',
-          url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
-        }]
+        }
       }
     },
     methods: {
       submitForm (formName) {
         this.$refs[formName].validate((valid) => {
           if (valid) {
-            console.log(this.ruleForm)
-            console.log(this.value)
+            let form = {
+              title: this.ruleForm.title,
+              top: this.ruleForm.top,
+              postcategory: this.ruleForm.postcategory,
+              brief: this.ruleForm.brief,
+              extended: this.ruleForm.extended,
+              image: this.ruleForm.image
+            }
+            console.log(form)
+            let url = '/api/admin/post/add'
+            let that = this
+            that.$axios.post(url, form).then(function (res) {
+              console.log(`查询ok`)
+              if (res.status === 200 && res.data.status === 200) {
+                alert('添加成功')
+              } else {
+                alert('添加失败')
+              }
+            }).catch(function (err) {
+              console.log(`查询err: ${err}`)
+              console.log(err)
+              alert('添加失败')
+            })
           } else {
             console.log('error submit!!')
             return false
@@ -81,12 +91,6 @@
       },
       resetForm (formName) {
         this.$refs[formName].resetFields()
-      },
-      handleRemove (file, fileList) {
-        console.log(file, fileList)
-      },
-      handlePreview (file) {
-        console.log(file)
       }
     }
   }
@@ -94,7 +98,7 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .title, .brief {
+  .title, .brief, .image {
     max-width: 500px;
   }
   .upload-demo{
